343996ce2dc6a77b8f56da8f682794d32a0eaf31
Olya Asedach <olyaasedach@MacBook-Air-Olya.local>
2026-07-26 10:14:54 +0300

    Remove extra bracket in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCmbVwwSd5GjyhY1MkF_oSkB7SEaORgLyU",
-authDomain: "flow-space-theta.vercel.app",
+  authDomain: "flow-space-theta.vercel.app",
   projectId: "matrix-hr-3ba0a",
   storageBucket: "matrix-hr-3ba0a.firebasestorage.app",
   messagingSenderId: "960958272528",
@@ -15,4 +15,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(); // Создали и экспортируем провайдер Google
 export const db = getFirestore(app);
-}
